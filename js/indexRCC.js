87c95979d74ebe13ac9c757803e462c198149078
Renato Correa <renatocorrea@targettrust.local>
2018-11-14 11:46:05 -0200

    salvando arquivos

---
+++ js/indexRCC.js
@@ -0,0 +1,9 @@
+function soma (){
+    var result = parseInt(document.getElementById("txtNumber1").value) + parseInt(document.getElementById("txtNumber2").value);
+    alert("Resultado: " + result);
+}
+
+function subtrai (){
+    var result = parseInt(document.getElementById("txtNumber1").value) - parseInt(document.getElementById("txtNumber2").value);
+    alert("Resultado: " + result);
+}
