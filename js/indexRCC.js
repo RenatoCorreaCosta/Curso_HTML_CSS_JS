b80219e335d5d221e562b96f32394e330c40c345
Renato Correa <renatocorrea@targettrust.local>
2018-11-19 09:27:28 -0200

    mais arquivos

--- js/indexRCC.js
+++ js/indexRCC.js
@@ -9,3 +9,44 @@ function subtrai (){
     alert("Resultado: " + result);
     console.log("Resultado: " + result);
 }
+
+//exSwtichCase
+function getSexo(){
+    var results = document.getElementsByName("sexo");
+    var finalResult;
+
+    for (i = 0; i < results.length; i++) {
+        if (results[i].type == "radio" && results[i].checked) {
+            finalResult = results[i].value;
+        }
+    }
+
+    alert("Resultado:" + finalResult);
+
+    /*
+    switch (new date().getDay()) {
+        case 1:
+            result = "Segunda";
+            break;
+        case 2:
+            result = "Terça";
+            break;
+        default:
+            result = "Sem definição"
+    }*/
+}
+
+//exSwtichCase
+/*function getDiaSemana{
+    var result = "";
+    switch (new date().getDay()) {
+        case 1:
+            result = "Segunda";
+            break;
+        case 2:
+            result = "Terça";
+            break;
+        default:
+            result = "Sem definição"
+    }
+}*/
